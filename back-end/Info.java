199af3d08bc36c4fac949d00b34aba53492c1969
Sean Cano <sean-anthony.cano.622@my.csun.edu>
2025-04-22 15:04:09 -0700

    Added my Info file, still working on it so it can pull data from other classes

---
+++ back-end/Info.java
@@ -0,0 +1,28 @@
+import java.util.UUID;
+/**
+ * Stores cusomer info.
+ * <p>
+ * Stores all of the customer's info that include seat numbers, flight info, customer account info, etc.
+ * Date:
+ * @author X
+ */
+class Info {
+    public Info(String cusName, String cusEmail, String cusFlightNum, String cusDepartureCity, 
+    String cusArrivalCity, String cusDepartureTime, String cusSeatNumber, String cusSeatClass) {
+        cusName = getName();
+        cusEmail = getEmail();
+        cusFlightNum = getFlightNum();
+        cusDepartureCity = getDepartureCity();
+        cusArrivalCity = getArrivalCity();
+        cusDepartureTime = getDepartureTime();
+        cusSeatNumber = getSeatNumber();
+        cusSeatClass = getSeatClass();
+    }
+}
+
+public class main {
+    public static void main(String[] args) {
+        ArrayList<Info> cusInfo = new ArrayList<>();
+        cusInfo.add(new Info());
+    }
+}
